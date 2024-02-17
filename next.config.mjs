4e1489a752d42b67be955dd2083c5e5b1f0655ad
Engine_Machiner <mfpc98@gmail.com>
2024-02-17 00:56:45 -0500

    Remove and test basePath

--- next.config.mjs
+++ next.config.mjs
@@ -11,13 +11,6 @@ const nextConfig = {
     output: "export",
 
     /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/",
-
-    /**
      * Disable server-based image optimization. Next.js does not support
      * dynamic features with static exports.
      *
